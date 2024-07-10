71cbaccfbd21903c2bacfa53229d20d5a698491c
Siggless <157992406+Siggless@users.noreply.github.com>
2024-07-10 15:26:44 +0100

    Add files via upload

--- ftexplorer/gui.py
+++ ftexplorer/gui.py
@@ -26,9 +26,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import re
 import qdarkgraystyle
 from bpdeditor.bpd_gui import BPDWindow
-from . import data, data_display
+from . import data
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 class MainTree(QtWidgets.QTreeView):
@@ -119,6 +120,227 @@ class MainTree(QtWidgets.QTreeView):
         # Select the item, if we found one.
         if found_path:
             self.setCurrentIndex(current.index())
+
+class DataDisplay(QtWidgets.QTextEdit):
+    """
+    Display area for our data
+    """
+
+    # Syntax Highlighting color definitions.
+    colors = {
+
+        # Default (light, probably) theme
+        False: {
+                'quotes': 'darkgoldenrod',
+                'names': 'mediumblue',
+                'headers': 'darkgreen',
+                'numbers': 'darkred',
+                'bools': 'darkviolet',
+            },
+
+        # Dark Theme
+        True: {
+                'quotes': 'palegoldenrod',
+                'names': 'lightblue',
+                'headers': 'lawngreen',
+                'numbers': 'palevioletred',
+                'bools': 'violet',
+            },
+        }
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.initial_display()
+        self.setReadOnly(True)
+        self.search_str = None
+
+        # Use a Monospaced font
+        font = QtGui.QFont(self.parent.settings.value('mainwindow/datafont', 'Monospace'))
+        fontsize = self.parent.settings.value('mainwindow/datafontsize')
+        try:
+            if fontsize:
+                font.setPointSizeF(float(fontsize))
+        except ValueError:
+            pass
+        font.setStyleHint(font.Monospace)
+        self.setFont(font)
+
+        # Default to not word-wrapping
+        self.setWordWrapMode(QtGui.QTextOption.NoWrap)
+
+    def initial_display(self):
+        """
+        Clears out output
+        """
+        self.node = None
+        self.setText('(nothing selected)')
+
+    def setText(self, text, clear_node=True):
+        """
+        Sets text
+        """
+        if clear_node:
+            self.node = None
+        super().setText(text)
+
+    def setHtml(self, text, clear_node=True):
+        """
+        Sets HTML
+        """
+        if clear_node:
+            self.node = None
+        super().setHtml(text)
+
+    def setPlainText(self, text, clear_node=True):
+        """
+        Sets plain text
+        """
+        if clear_node:
+            self.node = None
+        super().setPlainText(text)
+
+    def setNode(self, node):
+        """
+        Sets our currently-shown node, which will take into account our
+        multiline option.
+        """
+        self.node = node
+        self.updateText()
+
+    def updateText(self):
+        """
+        Updates the text that we're showing, taking into account our
+        multiline option.
+        """
+        # Only update if we have a node
+        if self.node:
+
+            do_multiline = self.parent.toolbar.action_multiline.isChecked()
+            if do_multiline:
+                # This is all pretty hacky, but seems to work fine.
+                output = []
+                for line in self.node.load():
+                    indent_level = 0
+                    parts = line.split('=', 1)
+                    if len(parts) == 1:
+                        output.append(line)
+                    else:
+                        chars = [char for char in parts[0]]
+                        chars.append('=')
+                        for char in parts[1]:
+                            if char == '(':
+                                indent_level += 1
+                                chars.append(char)
+                                chars.append("\n")
+                                output.append(''.join(chars))
+                                chars = [' '*((indent_level+1)*4)]
+                            elif char == ')':
+                                if indent_level > 0:
+                                    indent_level -= 1
+                                chars.append("\n")
+                                output.append(''.join(chars))
+                                chars = [' '*((indent_level+1)*4)]
+                                chars.append(char)
+                            elif char == ',' and indent_level > 0:
+                                chars.append(char)
+                                chars.append("\n")
+                                output.append(''.join(chars))
+                                chars = [' '*((indent_level+1)*4)]
+                            else:
+                                chars.append(char)
+                        output.append(''.join(chars))
+            else:
+                output = [line for line in self.node.load()]
+
+            # Apply syntax highlighting.  This is pretty hokey as well, but
+            # seems to work well enough.  Ideally we should be *actually*
+            # parsing things, but whatever.  Because we're just throwing a
+            # bunch of regexes at the text, the order is important; our
+            # conversion from <,> to &lt;,&gt; has to happen first, since
+            # otherwise it'd strip out the HTML we put in; and the quotes
+            # have to be processed next, as well.
+            do_syntax = self.parent.toolbar.action_syntax.isChecked()
+            colors = self.colors[self.parent.toolbar.action_dark.isChecked()]
+            for (idx, line) in enumerate(output):
+
+                # Get rid of anything which could be considered HTML by accident.
+                # (some descriptions, like GD_Aster_ClapTrapBeard.M_ClapTrapBeard, use
+                # HTML like <br>).  Do this regardless of syntax highlighting.
+                output[idx] = output[idx].replace('<', '&lt;')
+                output[idx] = output[idx].replace('>', '&gt;')
+
+                if do_syntax:
+
+                    # See if we have an assignment of some sort in here
+                    have_assignment = '=' in output[idx]
+
+                    # Colorize anything in quotes
+                    dostuff = False
+                    if self.node.name == 'WillowWaypoint_6' and 'KillJackSet' in output[idx]:
+                        dostuff = True
+                    output[idx] = re.sub(
+                            '(["\'])(.*?)\\1',
+                            r'<font color="{}">\1\2\1</font>'.format(colors['quotes']),
+                            output[idx])
+
+                    # Make the lefthand side of any assignment blue
+                    if have_assignment:
+                        output[idx] = re.sub(
+                                r'^(\s+)([^=]+?)=',
+                                r'\1<font color="{}">\2</font>='.format(colors['names']),
+                                output[idx])
+
+                    # Section headers in green
+                    output[idx] = re.sub(
+                            r'^=== (.*) ===',
+                            r'<font color="{}">=== \1 ===</font>'.format(colors['headers']),
+                            output[idx])
+
+                    # Numbers in red
+                    output[idx] = re.sub(
+                            r'\((\d+)\)',
+                            r'(<font color="{}">\1</font>)'.format(colors['numbers']),
+                            output[idx])
+                    output[idx] = re.sub(
+                            r'=(-?[0-9\.]+)',
+                            r'=<font color="{}">\1</font>'.format(colors['numbers']),
+                            output[idx])
+
+                    # Booleans/Nones in purple, I guess
+                    output[idx] = re.sub(
+                            r'=(none|true|false)',
+                            r'=<font color="{}">\1</font>'.format(colors['bools']),
+                            output[idx],
+                            flags=re.I)
+
+                # Also turn any initial spaces into &nbsp;  Do this regardless
+                # of syntax highlighting
+                space_count = 0
+                for char in output[idx]:
+                    if char == ' ':
+                        space_count += 1
+                    else:
+                        break
+                if space_count > 0:
+                    output[idx] = '{}{}'.format('&nbsp;'*space_count, output[idx][space_count:])
+
+            # Display
+            self.setHtml('<br>'.join(output), clear_node=False)
+
+    def search_for(self, search_str):
+        """
+        Searches for text inside our currently-displayed stuff
+        """
+        self.search_str = search_str
+        self.find(search_str)
+
+    def search_next(self):
+        """
+        Searches for the next instance of our previously-searched text
+        """
+        if self.search_str:
+            self.find(self.search_str)
 
 class GameSelect(QtWidgets.QComboBox):
     """
@@ -245,7 +467,7 @@ class GUI(QtWidgets.QMainWindow):
         self.splitter = QtWidgets.QSplitter()
 
         # Set up our display area and add it to the hbox
-        self.display = data_display.DataDisplay(self)
+        self.display = DataDisplay(self)
 
         # Set up our treeview
         current_game = self.settings.value('toggles/game', 'bl2')
